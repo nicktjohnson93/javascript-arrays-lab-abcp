2ea7317f21b3c9158496de319d14f89cd110ec2c
Nick Johnson <nicktjohnson93@gmail.com>
2018-01-19 03:20:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 const app = "I don't do much."
+var kittens = ["Milo", "Otis","Garfield"]
+
+function destructivelyAppendKitten(string, array){
+  var newKittens = kittens
+  return newKittens.push(string)
+}
+
+function destructivelyPrependKitten(string, array){
+  var newKitten = kittens
+  return newKitten.unshift(string)
+}
+
+function destructivelyRemoveLastKitten(string, array){
+  var newKitty = kittens
+  return newKitty.pop(string)
+}
+
+function destructivelyRemoveFirstKitten(string, array){
+  var newKit = kittens
+  return newKit.shift(string)
+}
+
+function appendKitten(string, array){
+  var newArray = kittens
+  return [...newArray, string]
+}
+
+function prependKitten(string, array){
+  var newKitt = kittens
+  return [string, ...newKitt]
+}
+
+function removeLastKitten(array){
+  var newArrays = kittens
+  return newArrays.slice(0, newArrays.length -1)
+}
+
+function removeFirstKitten(array){
+  var newKitts = kittens
+  return newKitts.slice(1)
+}

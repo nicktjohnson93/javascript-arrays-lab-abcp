52e7b2588df705aa860d333e40b28a17db498cd5
Nick Johnson <nicktjohnson93@gmail.com>
2018-01-19 03:00:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = "I don't do much."
 var kittens = ["Milo", "Otis","Garfield"]
 
-function destructivelyAppendKitten(string, array){
+destructivelyAppendKitten = appendKittens(string, array){
   return appendArray(string)
 }
